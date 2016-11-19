ef84497f6dd315fd8f470876593e433876079628
Alexander Alemayhu <alexander@alemayhu.com>
2016-11-19 14:39:03 +0100

    client: use file descriptors
    Also added some future todo.
    
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- hello-client.c
+++ hello-client.c
@@ -1,14 +1,34 @@
 #include <linux/bus1.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define CHAR_DEVICE "/dev/bus1"
 
 int main(int argc, const char *argv[])
 {
 	printf("hello client");
 
-	struct bus1_peer *peer = bus1_peer_new();
+	int peer = open(CHAR_DEVICE, O_RDONLY);
+	if (0 > peer) {
+		fprintf(stderr, "failed to open the character device %s\n",
+			CHAR_DEVICE);
+		return EXIT_FAILURE;
+	}
 
-	bus1_peer_free(peer);
+	bool incoming_message = false;
+	while (incoming_message) {
+		char *msg = "";
+		// TODO: Read messages from CHAR_DEVICE
+		if (!strcmp(msg, "quit"))
+			break;
+		printf("> %s", msg);
+	}
+	close(peer);
 
 	return EXIT_SUCCESS;
 }
